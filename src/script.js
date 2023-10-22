2251dfc0edf6a1b131bfd8a762e0f6e74dd37ceb
JoyEmma12 <ejoy0600@gmail.com>
2023-10-22 16:08:39 +0100

    completed and interactive

---
+++ src/script.js
@@ -0,0 +1,22 @@
+let subscribe = document.getElementById("subscribe");
+subscribe.addEventListener("click", function () {
+    let email = document.getElementById("email").value;
+
+    if (email) {
+        document.getElementById("thanks").style.display = "block";
+        document.getElementById('sub-containers').style.display = 'none';
+    }
+
+    let paragraph = document.getElementById('confirmMessage');
+    paragraph.innerHTML = `A confirmation mail has been sent to ${email}. Pls open it and click on button inside to confirm your subscription`;
+})
+
+document.getElementById('thanks').style.display = "none";
+
+/*for dismiss*/
+
+let dismissMessage = document.getElementById('dismissMessage');
+dismissMessage.addEventListener('click', function () {
+    document.getElementById('sub-containers').style.display = 'flex';
+    document.getElementById('thanks').style.display = 'none'
+})
